345c3da600ca0673a4e18e11fd71cdbc320b9b29
Akash Raj <akashraj23864@gmail.com>
2026-06-08 11:19:20 +0530

    guru99

--- Capstone_Project/src/test/java/utilities/ExcelUtils.java
+++ Capstone_Project/src/test/java/utilities/ExcelUtils.java
@@ -7,7 +7,7 @@ import org.apache.poi.xssf.usermodel.*;
 public class ExcelUtils {
 
 	public static String getData(String sheet,int row,int cell)throws Exception {
-		FileInputStream fis =new FileInputStream("C:\\guru99xl\\project.xlsx");
+		FileInputStream fis =new FileInputStream("project.xlsx");
 		XSSFWorkbook wb =new XSSFWorkbook(fis);
 		XSSFSheet sh = wb.getSheet(sheet);
 		return sh.getRow(row).getCell(cell).getStringCellValue();
